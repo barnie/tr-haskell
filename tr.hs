1ad0b6112af1bc47ada526b843f43d91b47375d5
Piotr Kruk <piotr@kruk.co>
2014-12-28 17:40:32 +0100

    first simple translation

--- tr.hs
+++ tr.hs
@@ -1,34 +1,18 @@
-
-import Control.Monad (unless)
+import System.Environment
 import System.IO (isEOF)
 
+--args function
+version = putStr "version 1.0\nauthor: Piotr Kruk\nemail: piotr@kruk.co\n"
 
-replace1::(Eq a)=>a->a->[a]->[a]
-replace1 _ _ [] = []
-replace1 x y (f:fg) = if x == f then [y] ++ (replace1 x y fg) else [f] ++ (replace1 x y fg)
+-- function to help
 
---translate [] [] lista = lista
---translate (f:fx) (g:gx) lista = translate fx gx (replace1 f g lista)
---nie dziala dla "abc" "bcd"
---
 ifExist x [] = []
 ifExist x (f:fg) = if ( x == (fst f)) then [f] else ifExist x fg
 
 translate _ [] = []
 translate ds (f:fg) = if (null (ifExist f ds)) == True then [f] ++ (translate ds fg) else [ ( snd (head (ifExist f ds)) ) ] ++ (translate ds fg)
 
-prepareTranslation::(Show a, Eq a)=>[a]->[a]->[a]->[a]
 prepareTranslation aa bb to_translate = translate (zip aa bb) to_translate
-
-processEachLine :: (String -> IO a) -> IO ()
-processEachLine k = do
-        finished <- isEOF
-        unless finished $ do
-                k =<< getLine
-                processEachLine k
-
-main :: IO ()
-main = helper(["abc", "ABC"]) --initial state is empty string
 
 helper ::(Show a, Eq a)=>[a] -> IO ()
 helper initialState = do end <- isEOF
@@ -40,5 +24,17 @@ helper initialState = do end <- isEOF
                                      putStrLn (prepareTranslation a z linein )
                                      helper(initialState)
 
-transition :: (String, String) -> (String, String)
-transition inpair = (nextState, nextState) where nextState = (snd inpair) ++ (fst inpair)
+
+main = do
+    args <- getArgs
+    if args == ["-v"] then version
+    else if length(args) == 2 then (helper args)
+    else print $ args
+
+
+myLoop = do done <- isEOF
+            if done
+              then putStrLn "Bye!"
+              else do inp <- getLine
+                      putStrLn (inp)
+                      myLoop
