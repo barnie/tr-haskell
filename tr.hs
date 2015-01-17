69ba5b868433a98d5d3359d640884238ff1a5130
Piotr Kruk <piotr@kruk.co>
2015-01-17 16:12:42 +0100

    add truncate option to tr

--- tr.hs
+++ tr.hs
@@ -4,7 +4,7 @@ import System.IO (isEOF)
 --args function
 version = putStr "version 1.0\nauthor: Piotr Kruk\nemail: piotr@kruk.co\n"
 
--- function to help
+-- if exist in list
 
 ifExist x [] = []
 ifExist x (f:fg) = if ( x == (fst f)) then [f] else ifExist x fg
@@ -14,17 +14,45 @@ translate ds (f:fg) = if (null (ifExist f ds)) == True then [f] ++ (translate ds
 
 prepareTranslation aa bb to_translate = translate (zip (checkArgs ' ' aa) (checkArgs ' ' bb)) to_translate
 
+--there is a problem with conversion to string with show (sometimes i get ''abc or abc")
+cutit [] = []
+cutit (f:fg) = if f == '"' then cutit fg else [f] ++ cutit fg
+
+-- -t (truncate option)
+
 helper ::(Show a, Eq a)=>[a] -> IO ()
 helper initialState = do end <- isEOF
                          if end
                              then putStr ""
                              else do linein <- getLine
-                                     let a = show (initialState!!0)
-                                     let z = show (initialState !! 1)
-                                     putStrLn (prepareTranslation a z linein )
+                                     let a = cutit (show (initialState!!0))
+                                     let z = cutit (show (initialState !! 1))
+                                     putStrLn ((prepareTranslation a z linein) )
+                                     helper(initialState)
+--get last element from list
+listLast::[a]->a
+listLast [x] = x
+listLast(_:xs) = listLast xs
+--return list of N last elements
+complete::(Eq b, Num b)=>[a]->b->[a]
+complete _ 0 = []
+complete x y = [ listLast x ] ++ (complete x (y - 1))
+
+--normal translation
+transl ::(Show a, Eq a)=>[a] -> IO ()
+transl initialState = do end <- isEOF
+                         if end
+                             then putStr ""
+                             else do linein <- getLine
+                                     let a = cutit (show (initialState!!0))
+                                     let z = cutit (show (initialState !! 1))
+                                     if (length(a) <= length(z)) then
+                                      putStrLn ((prepareTranslation a z linein) )
+                                     else
+                                      putStrLn ((prepareTranslation ( a ++ ( complete a (length(z) - length(a)) ) ) z linein) )
                                      helper(initialState)
 
-
+--chech arguments for dictionary
 checkArgs x [] = []
 checkArgs prev (f:fg) =
   if f == '-' then
@@ -35,6 +63,7 @@ checkArgs prev (f:fg) =
     [f]
   else
     checkArgs f fg
+
 --replace
 replace :: Eq a => [a] -> [a] -> [a] -> [a]
 replace [] _ _ = []
@@ -42,6 +71,8 @@ replace s find repl =
     if take (length find) s == find
         then repl ++ (replace (drop (length find) s) find repl)
         else [head s] ++ (replace (tail s) find repl)
+
+
 --remove all characters
 deleter::[Char] -> IO ()
 deleter initialState = do end <- isEOF
@@ -92,11 +123,20 @@ complet initialState = do end <- isEOF
                                      let z = (snd initialState) ++ result
                                      complet(args, z)
 
+--dictionaries
+checkDic ":alnum:" = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
+checkDic ":alpha:" = ['a'..'z'] ++ ['A'..'Z']
+checkDic ":digit:" = ['0'..'9']
+checkDic ":lower:" = ['a'..'z']
+checkDic ":upper:" = ['A'..'Z']
+checkDic x = x
+
 main = do
     args <- getArgs
     if args == ["-v"] then version
+    else if head (args) == "-t" then ( helper (tail args) )
     else if head (args) == "-d" then deleter (convert_arr_string_to_string(tail args))
     else if head (args) == "-s" then remove1 (head (convert_arr_string_to_string(tail args)))
     else if head (args) == "-c" || head(args) == "-C" then complet ([args !! 1, args !! 2], "")
-    else if length(args) == 2 then (helper args)
+    else if length(args) == 2 then (transl args)
     else print $ args
